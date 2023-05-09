73bcf04bb23fd708b387442f07d85e6deebf6674
AliceWxy <76913427+AliceWxy@users.noreply.github.com>
2023-05-09 11:33:20 +0800

    Update code.R

--- code.R
+++ code.R
@@ -1,1 +1,11 @@
+# Import the raw data and colnames
+```{r}
+load(url("https://github.com/zhentaoshi/Econ5821/raw/main/data_example/dataset_inf.Rdata"))
+#Column names
+X_colnames <- read.csv("https://github.com/zhentaoshi/Econ5821/raw/main/data_example/X_colnames.csv")
+```
 
+# Rename the colnames
+```{r}
+colnames(X) = X_colnames$X
+```
